60ed80ec12b729855cd62cbe5c13295dd3251aff
Eduardo Alvim de Carvalho <eduardoac@gmail.com>
2016-05-07 20:36:20 -0300

    Commit inicial

---
+++ src/main/java/br/com/iworks/spring/cloud/server/app/Application.java
@@ -0,0 +1,14 @@
+package br.com.iworks.spring.cloud.server.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+}
